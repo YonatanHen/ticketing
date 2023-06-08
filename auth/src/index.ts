0ee410441aece8f0f489dad1e102f66813460eb1
Yonatan <yonatanh@jfrog.com>
2023-06-08 16:36:09 +0300

    creating routes + updating k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express  from "express";
 import { json } from "body-parser";
 
+import { currentuserRouter } from "./routes/current-user";
+
 const app = express();
 app.use(json());
 
+app.use(currentuserRouter);
+
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log('Listening on port 3000!');
 });
