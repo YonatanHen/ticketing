f4469626d6175d1f1969fbcbd35e8ba0ae9f3074
yonatanh <yonatanh@jfrog.com>
2023-07-15 16:40:54 +0300

    fixed endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req,res) => {
+router.get('/api/users/currentuser', (req,res) => {
     res.send('Hi there!');
 });
 
