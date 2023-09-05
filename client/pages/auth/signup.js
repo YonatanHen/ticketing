70aa9c2f7da1b0ce3751bd6f4810eb81dd4cc593
yonatanh <yonatanh@jfrog.com>
2023-09-05 16:31:49 +0300

    added signup logic

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,40 @@
+import { useState } from 'react'
+import axios from 'axios'
+
 export default () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('/api/users/signup', {
+                email, password
+            })
+       } catch (err) {
+            setErrors(err.response.data.errors);
+       }
+    }
+
     return (
-    <form>
+    <form onSubmit={onSubmit}>
         <h1>Sign Up</h1>
         <div className="form-group">
             <label>Email Address</label>
-            <input className="form-control"/>
+            <input value={email} onChange={e => setEmail(e.target.value)} className="form-control"/>
         </div>
         <div className="form-group">
             <label>Password</label>
-            <input type="password" className="form-control" />
+            <input value={password} onChange={e => setPassword(e.target.value)}  type="password" className="form-control" />
         </div>
+        {errors.length > 0 && <div className='alert alert-danger'>
+            <h4>Ooops....</h4>
+            <ul className='my-0'/>
+            {errors.map(err => <li key={err.message}>{err.message}</li>)}    
+        </div>}
+    
         <button className="btn btn-primary">Sign Up</button>
     </form>
     )
