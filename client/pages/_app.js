99377e87b42d870f631027fed8950360b7367c4f
yonatanh <yonatanh@jfrog.com>
2023-09-14 14:46:39 +0300

    edited the _app.js file

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+    return <div>
+        <h1>Header!</h1>
+        <Component {...pageProps} />
+    </div>
 };
 
