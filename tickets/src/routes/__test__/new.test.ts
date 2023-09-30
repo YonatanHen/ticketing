9831d0b247b6567483897d3c9156bc4f8d8bbd83
yonatanh <yonatanh@jfrog.com>
2023-09-30 22:20:47 +0300

    edited connection to mongo + created tests folder

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import { Request } from "supertest";
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+
+});
+
+it('can only be accessed if the user is signed in', async () => {
+    
+});
+
+it('returns an error if an invalud title is provided', async () => {
+    
+});
+
+it('returns an error if an invalid price is provided', async () => {
+    
+});
+
+it('creates a ticket with invalid inputs', async () => {
+    
+});
