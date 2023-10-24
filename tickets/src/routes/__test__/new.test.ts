b1f204b8e408519c5139c46a09b364c4628a90dd
yonatanh <yonatanh@jfrog.com>
2023-10-24 17:10:29 +0300

    created more tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,22 +1,75 @@
-import { Request } from "supertest";
+import request from "supertest";
 import { app } from '../../app';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
+    const response = await request(app)
+    .post('/api/tickets')
+    .send({})
 
+    expect(response.status).not.toEqual(404);
 });
 
 it('can only be accessed if the user is signed in', async () => {
-    
+    await request(app)
+    .post('/api/tickets')
+    .send({})
+    .expect(401);
 });
 
-it('returns an error if an invalud title is provided', async () => {
-    
+it('the user returns other status than 401 if the user is signed in', async () => {
+    const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
+
+    expect(response.status).not.toEqual(401);
+});
+
+it('returns an error if an invalid title is provided', async () => {
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: '',
+        price: 10
+    })
+    .expect(400);
+
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        price: 10
+    })
+    .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
-    
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'asdssss',
+        price: -10 
+    })
+    .expect(400);
+
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'ladsadsad'
+    })
+    .expect(400);
 });
 
 it('creates a ticket with invalid inputs', async () => {
     
+    await request(app)
+    .post('api/tickets')
+    .send({
+        title: 'asasasfd',
+        price: 200
+    })
+    expect(201)
 });
