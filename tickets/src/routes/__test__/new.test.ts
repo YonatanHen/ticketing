e46666ff07be798b31861894123ee7e44576fbe3
yonatanh <yonatanh@jfrog.com>
2023-10-31 11:44:13 +0200

    created new tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
     const response = await request(app)
@@ -64,12 +65,22 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with invalid inputs', async () => {
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+
+    const title = 'asass';
     
     await request(app)
-    .post('api/tickets')
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
     .send({
-        title: 'asasasfd',
-        price: 200
+        title,
+        price: 20
     })
     expect(201)
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].price).toEqual(20);
+    expect(tickets[0].title).toEqual(title);
 });
