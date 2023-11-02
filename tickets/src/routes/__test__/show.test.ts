783c4f85ae500ee1b7087ed936383819a4ee32ba
yonatanh <yonatanh@jfrog.com>
2023-11-02 14:07:35 +0200

    added new tests

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,11 +1,14 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
 it('returns a 404 if the ticket is not found', async () => {
-    const res = await request(app)
-    .get('/api/tickets/lsfsdadlsasl')
+    const id = new mongoose.Types.ObjectId().toHexString();
+
+    await request(app)
+    .get(`/api/tickets/${id}`)
     .send()
-    .expect(404);
+    .expect(404)
 });
 
 it('returns the ticket if the ticket is found', async () => {
